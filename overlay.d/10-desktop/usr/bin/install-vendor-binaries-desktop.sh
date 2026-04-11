d4b48376aff2b716d82e52e1388b0098320ef431
aleskandro <mail@alessandrodistefano.eu>
2026-04-11 11:26:40 +0100

    vendor binaries fix

--- overlay.d/10-desktop/usr/bin/install-vendor-binaries-desktop.sh
+++ overlay.d/10-desktop/usr/bin/install-vendor-binaries-desktop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-source /etc/zshrc
+. /usr/bin/_common_vendor_binaries.zsh
 
 [ "$(id -u)" = "0" ] && LOCAL_BIN_PATH=/usr/local/bin || LOCAL_BIN_PATH=~/.local/bin
 LLVM_ARCH="$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')"
